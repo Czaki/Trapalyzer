5dc8f8ad7c4b871e58c375fff23b65f1ecd51680
Grzegorz Bokota <bokota+github@gmail.com>
2020-11-12 16:21:11 +0100

    update segmentaion algorithm

--- partseg_plugin/neutrofile_plugin/segmentation.py
+++ partseg_plugin/neutrofile_plugin/segmentation.py
@@ -32,52 +32,35 @@ class NeutrofileSegmentation(RestartableAlgorithm):
         thr1, thr2 = thr
         return (data > thr1) * (data < thr2)
 
-    def calculation_run(self, report_fun: Callable[[str, int], None] = None) -> SegmentationResult:
-        for key in self.new_parameters.keys():
-            self.parameters[key] = deepcopy(self.new_parameters[key])
-        channel = self.get_channel(self.new_parameters["dna_marker"])
-        thr: BaseThreshold = threshold_dict[self.new_parameters["threshold"]["name"]]
-        inner_dna_mask, thr_val = thr.calculate_mask(
-            channel, self.mask, self.new_parameters["threshold"]["values"], operator.gt
+    def _calculate_mask(self, channel, threshold_name):
+        thr: BaseThreshold = threshold_dict[self.new_parameters[threshold_name]["name"]]
+        mask, thr_val = thr.calculate_mask(
+            channel, self.mask, self.new_parameters[threshold_name]["values"], operator.gt
         )
-        _mask, alive_thr_val = thr.calculate_mask(
-            channel, self.mask, self.new_parameters["threshold2"]["values"], operator.gt
-        )
-        dead_channel = self.get_channel(self.new_parameters["dead_dna_marker"])
-        thr2: BaseThreshold = threshold_dict[self.new_parameters["dead_threshold"]["name"]]
-        out_dna_mask, dead_thr_val = thr2.calculate_mask(
-            dead_channel,
-            self.mask,
-            self.new_parameters["dead_threshold"]["values"],
-            operator.gt,
-        )
+        return mask, thr_val
 
-        out_dna_components = sitk.GetArrayFromImage(
+    @staticmethod
+    def _calc_components(arr, min_size):
+        return sitk.GetArrayFromImage(
             sitk.RelabelComponent(
-                sitk.ConnectedComponent(sitk.GetImageFromArray(out_dna_mask)),
-                self.new_parameters["net_size"],
+                sitk.ConnectedComponent(sitk.GetImageFromArray(arr)),
+                min_size,
             )
         )
+    
+    @staticmethod
+    def _remove_net_components(channel, components, min_reach_thr):
+        for val in np.unique(components):
+            if val == 0:
+                continue
+            mask = components == val
+            if np.max(channel[mask]) < min_reach_thr:
+                components[mask] = 0
+        return components
 
-        _inner_dna_mask = inner_dna_mask.copy()
-
-        inner_dna_mask[out_dna_components > 0] = 0
-
-        inner_dna_components = sitk.GetArrayFromImage(
-            sitk.RelabelComponent(
-                sitk.ConnectedComponent(sitk.GetImageFromArray(inner_dna_mask)),
-                self.new_parameters["minimum_size"],
-            )
-        )
-
-        idn = inner_dna_components.copy()
-
-
-
-        odn = out_dna_components.copy()
-
-        # profile_info = MeasurementProfile.get_segmentation_to_mask_component(inner_dna_components, out_dna_components)
-
+    def _classify_neutrofile(self, inner_dna_components, out_dna_mask):
+        dna_channel = self.get_channel(self.new_parameters["dna_marker"])
+        _mask, alive_thr_val = self._calculate_mask(dna_channel, "threshold2")
         result = np.zeros(inner_dna_components.shape, dtype=np.uint8)
         self.good, self.bad, self.nets, self.bacteria, self.net_area = 0, 0, 0, 0, 0
         sizes = np.bincount(inner_dna_components.flat)
@@ -90,7 +73,7 @@ class NeutrofileSegmentation(RestartableAlgorithm):
             mm = inner_dna_components == val
             count = np.sum(out_dna_mask[mm])
 
-            mean_brightness = np.mean(channel[mm])
+            mean_brightness = np.mean(dna_channel[mm])
             if sizes[val] < max_size:
                 if count < ratio * sizes[val]:
                     if mean_brightness > alive_thr_val:
@@ -106,9 +89,36 @@ class NeutrofileSegmentation(RestartableAlgorithm):
                 result[mm] = BACTERIA_VAL
                 self.bacteria += 1
             else:
+                result[mm] = BACTERIA_VAL
+                self.bacteria += 1
                 print("problem here", val, sizes[val])
                 self.nets += 1
                 self.net_area += np.sum(mm)
+        return result
+
+
+    def calculation_run(self, report_fun: Callable[[str, int], None] = None) -> SegmentationResult:
+        for key in self.new_parameters.keys():
+            self.parameters[key] = deepcopy(self.new_parameters[key])
+        dna_channel = self.get_channel(self.new_parameters["dna_marker"])
+        dead_dna_channel = self.get_channel(self.new_parameters["dead_dna_marker"])
+        inner_dna_mask, thr_val = self._calculate_mask(dna_channel, "threshold")
+        out_dna_mask, dead_thr_val = self._calculate_mask(dead_dna_channel, "dead_threshold")
+        _mask, net_thr_val = self._calculate_mask(dead_dna_channel, "net_threshold")
+
+
+        out_dna_components = self._calc_components(out_dna_mask, self.new_parameters["net_size"])
+        out_dna_components = self._remove_net_components(dead_dna_channel, out_dna_components, net_thr_val)
+
+        _inner_dna_mask = inner_dna_mask.copy()
+
+        inner_dna_mask[out_dna_components > 0] = 0
+        inner_dna_components = self._calc_components(inner_dna_mask, self.new_parameters["minimum_size"])
+
+        idn = inner_dna_components.copy()
+        odn = out_dna_components.copy()
+
+        result =  self._classify_neutrofile(inner_dna_components, out_dna_mask)
         self.nets = np.max(out_dna_components)
         self.net_area = np.count_nonzero(out_dna_components)
         if self.new_parameters["separate_nets"]:
@@ -167,6 +177,13 @@ class NeutrofileSegmentation(RestartableAlgorithm):
                 possible_values=threshold_dict,
                 property_type=AlgorithmDescribeBase,
             ),
+            AlgorithmProperty(
+                "net_threshold",
+                "Net reach Threshold",
+                next(iter(threshold_dict.keys())),
+                possible_values=threshold_dict,
+                property_type=AlgorithmDescribeBase,
+            ),
             AlgorithmProperty("minimum_size", "Minimum size (px)", 20, (0, 10 ** 6)),
             AlgorithmProperty(
                 "maximum_neutrofile_size",
@@ -194,4 +211,21 @@ class NeutrofileSegmentation(RestartableAlgorithm):
             "name": ManualThreshold.get_name(),
             "values": {"threshold": 20},
         }
+        val["net_threshold"] = {
+            "name": ManualThreshold.get_name(),
+            "values": {"threshold": 34},
+        }
         return val
+
+
+class NeutrofileOnlySegmentation(RestartableAlgorithm):
+    def calculation_run(self, report_fun: Callable[[str, int], None]) -> SegmentationResult:
+        pass
+
+    @classmethod
+    def get_name(cls) -> str:
+        return "Neutrofile only"
+
+    @classmethod
+    def get_fields(cls) -> typing.List[typing.Union[AlgorithmProperty, str]]:
+        pass
